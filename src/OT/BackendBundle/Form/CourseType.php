0f61d360dd5e1d3bc4ec1229c9ec8a39a86f7416
Han Rui <betaver@gmail.com>
2014-09-01 00:03:34 +0800

    course crud finished

---
+++ src/OT/BackendBundle/Form/CourseType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OT\BackendBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CourseType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('course_id')
+            ->add('name')
+            ->add('description')
+            ->add('duration')
+            ->add('price')
+            ->add('status')
+            ->add('Category')
+            ->add('Users')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'OT\BackendBundle\Entity\Course'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ot_backendbundle_course';
+    }
+}
